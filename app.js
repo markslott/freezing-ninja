a41222023c340d893afc5e659ea057e07c76a5dc
Mark Lott <mlott@mlott-ltm1.internal.salesforce.com>
2015-06-15 11:55:24 -0500

    msl

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 
 app.get('/setup', function(req, res) {
 
