7efb8a2a3cf3b7613e3bfcfc8f655642a1bd5080
Mark Lott <mlott@mlott-ltm1.internal.salesforce.com>
2015-06-15 13:11:21 -0500

    added user

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+var User   = require('./models/user')
+
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/api', routes);
 
 
 // catch 404 and forward to error handler
