27ea48fd6ff8a5b74b200f11ed213bd756762aa6
Mark Lott <mlott@mlott-ltm1.internal.salesforce.com>
2015-06-15 12:34:56 -0500

    test

---
+++ routes/index.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/setup', function(req, res) {
+
+	// create a sample user
+	var nick = new User({ 
+		name: 'Mark Lott', 
+		password: 'password',
+		admin: true 
+	});
+	nick.save(function(err) {
+		if (err) throw err;
+
+		console.log('User saved successfully');
+		res.json({ success: true });
+	});
+});
+
+// basic route (http://localhost:8080)
+router.get('/test', function(req, res) {
+	res.send('Hello! welcome to my test api.');
+});
+
+
+// ---------------------------------------------------------
+// authentication (no middleware necessary since this isnt authenticated)
+// ---------------------------------------------------------
+// http://localhost:8080/api/authenticate
+router.post('/authenticate', function(req, res) {
+
+	// find the user
+	User.findOne({
+		name: req.body.name
+	}, function(err, user) {
+
+		if (err) throw err;
+
+		if (!user) {
+			res.json({ success: false, message: 'Authentication failed. User not found.' });
+		} else if (user) {
+
+			// check if password matches
+			if (user.password != req.body.password) {
+				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+			} else {
+
+				// if user is found and password is right
+				// create a token
+				var token = jwt.sign(user, app.get('superSecret'), {
+					expiresInMinutes: 1440 // expires in 24 hours
+				});
+
+				res.json({
+					success: true,
+					message: 'Enjoy your token!',
+					token: token
+				});
+			}		
+
+		}
+
+	});
+});
+
+// ---------------------------------------------------------
+// route middleware to authenticate and check token
+// ---------------------------------------------------------
+router.use(function(req, res, next) {
+
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+	// decode token
+	if (token) {
+
+		// verifies secret and checks exp
+		jwt.verify(token, app.get('superSecret'), function(err, decoded) {			
+			if (err) {
+				return res.json({ success: false, message: 'Failed to authenticate token.' });		
+			} else {
+				// if everything is good, save to request for use in other routes
+				req.decoded = decoded;	
+				next();
+			}
+		});
+
+	} else {
+
+		// if there is no token
+		// return an error
+		return res.status(403).send({ 
+			success: false, 
+			message: 'No token provided.'
+		});
+		
+	}
+	
+});
+
+// ---------------------------------------------------------
+// authenticated routes
+// ---------------------------------------------------------
+router.get('/', function(req, res) {
+	res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+router.get('/users', function(req, res) {
+	User.find({}, function(err, users) {
+		res.json(users);
+	});
+});
+
+router.get('/check', function(req, res) {
+	res.json(req.decoded);
+});
+
+
+module.exports = router;
