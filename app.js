594e79b6b441c6aa96591161608f62c5763d7bbd
Mark Lott <mlott@mlott-ltm1.internal.salesforce.com>
2015-06-15 11:04:52 -0500

    msl

--- app.js
+++ app.js
@@ -22,17 +22,12 @@ var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
 mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/setup', function(req, res) {
 
